02e5b960551a19ac4a01d7e6c1551fb39f010cb3
Tyler Hemmie <tylerhemmie@gmail.com>
2014-07-24 17:32:18 -0400

    I added the third and final view.

--- Homemortgagecalculator/FormulaListViewController.m
+++ Homemortgagecalculator/FormulaListViewController.m
@@ -29,29 +29,5 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*- (NSInteger)tableView:(UITableView *)tableView
-            numberOfRowsInSection:(NSInteger)section
-{
-    return 3;
-}
 
-- (UITableViewCell *)tableView:(UITableView *)tableView
-                cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    UITableViewCell *cell = [tableView
-                          dequeueReusableCellWithIdentifier:@"FormulaListItem"];
-    UILabel *label = (UILabel *)[cell viewWithTag:1000];
-    if (indexPath.row == 0) {
-        label.text = @"Mortgage";
-        [cell setBackgroundColor:[UIColor redColor]];
-    }
-        else if (indexPath.row == 1){
-        label.text = @"Front Ratio";
-    }
-        else if (indexPath.row == 2){
-        label.text = @"Back Ratio";
-    
-    }
-        return cell;
-}*/
 @end
